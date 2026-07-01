7771f21be0bd94e0dab4d9095a9be5b7fd1ebded
martireniquinaguirre418-cyber <martireniquinaguirre418@gmail.com>
2026-07-01 11:24:11 -0500

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,12 @@
+window.SM_WHATSAPP = "51992001097";
+window.ADMIN_PASSWORD = "SM-Privado-992001097";
+
+window.firebaseConfig = {
+  apiKey: "AIzaSyBP6-56THUXV6mkTy-Dwe7jA0ABF5BYVT4",
+  authDomain: "stream-masters-e9f03.firebaseapp.com",
+  projectId: "stream-masters-e9f03",
+  storageBucket: "stream-masters-e9f03.firebasestorage.app",
+  messagingSenderId: "489326345415",
+  appId: "1:489326345415:web:4eb72e68f31fb8dbdf52e2",
+  measurementId: "G-8FG10M3X3K"
+};
